17daf0f4f699fe5c81ec7b11606e3daac404e061
daniil <daniil@qqqq.myguest.virtualbox.org>
2025-04-28 03:25:07 +0300

    copy files without overwriting dublicates

--- collect_files.sh
+++ collect_files.sh
@@ -5,5 +5,15 @@ output_dir="$2"
 
 find "$input_dir" -type f | while read file; do
     filename=$(basename "$file")
-    cp "$file" "$output_dir/$filename"
+    target="$output_dir/$filename"
+    
+    if [[ -e "$target" ]]; then
+        count=1
+        while [[ -e "$output_dir/${filename%.*}$count.${filename##*.}" ]]; do
+            ((count++))
+        done
+        target="$output_dir/${filename%.*}$count.${filename##*.}"
+    fi
+
+    cp "$file" "$target"
 done
